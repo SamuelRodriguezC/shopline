d3ec906b779d1f08588d75212cb0ecb35e4dc6d2
SamuelRodriguezC <samuelrc250@gmail.com>
2025-07-16 10:58:02 -0500

    Perfil (Ordenes y lista de productos deseados)

---
+++ components/order/PurchasedOrder.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import OrderContainer from './OrderContainer'
+
+
+const PurchasedOrder = () => {
+  return (
+    <div className="main-max-width mx-auto padding-x">
+      <p className="font-semibold text-2xl max-sm:text-[16px] text-gray-800 my-4 text-center">
+        Tus Ordenes
+      </p>
+
+      <OrderContainer/>
+    </div>
+  )
+}
+
+export default PurchasedOrder
