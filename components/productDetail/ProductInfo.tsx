c54fecb2db379241bdc5ec2bc78d611b8a3478a1
SamuelRodriguezC <samuelrc250@gmail.com>
2025-07-23 11:59:37 -0500

    Añadir o quitar productos de la lista de deseados

--- components/productDetail/ProductInfo.tsx
+++ components/productDetail/ProductInfo.tsx
@@ -11,31 +11,32 @@ import { addToCartAction, addToWishlistAction } from '@/lib/actions'
 import { toast } from 'react-toastify'
 import WishlistTooltip from '../uiComponents/WishlistTooltip'
 
-const ProductInfo = ({product, loggedInUserEmail}: {product: ProductDetail, loggedInUserEmail: string | null | undefined}) => {
+const ProductInfo = ({ product, loggedInUserEmail }: { product: ProductDetail, loggedInUserEmail: string | null | undefined }) => {
 
   const { cartCode, setCartItemsCount } = useCart()
-  const [ addToCartLoader, setAddToCartLoader ] = useState(false)
-  const [ addedToWishList, setAddedToWishList] = useState(false)
-  const [ addedToCart , setAddedToCart ] = useState(false)
+  const [addToCartLoader, setAddToCartLoader] = useState(false)
+  const [addedToWishList, setAddedToWishList] = useState(false)
+  const [addedToCart, setAddedToCart] = useState(false)
+  const [addWishListLoader, setWishListLoader] = useState(false)
 
   useEffect(() => {
 
-    async function handleAddToCart(){
+    async function handleAddToCart() {
 
-      try{
+      try {
         const response = await api.get(`product_in_cart?cart_code=${cartCode}&product_id=${product.id}`)
         setAddedToCart(response.data.product_in_cart)
         return response.data
       }
-      catch(err: unknown){
-      if(err instanceof Error){
-        throw new Error(err.message)
+      catch (err: unknown) {
+        if (err instanceof Error) {
+          throw new Error(err.message)
+        }
+        throw new Error("Un Eror Desconocido ha Ocurrido")
       }
-      throw new Error("Un Eror Desconocido ha Ocurrido")
-    }
 
-  }
-  handleAddToCart()
+    }
+    handleAddToCart()
 
   }, [cartCode, product.id])
 
@@ -45,48 +46,52 @@ const ProductInfo = ({product, loggedInUserEmail}: {product: ProductDetail, logg
     const formData = new FormData();
     formData.set("cart_code", cartCode ? cartCode : "")
     formData.set("product_id", String(product.id))
-    
-    try{
+
+    try {
       const response = await addToCartAction(formData)
       setAddedToCart(true)
       setCartItemsCount(curr => curr + 1)
       toast.success("Producto Añadido al Carrito")
       return response
     }
-    catch(err: unknown){
-      if(err instanceof Error){
+    catch (err: unknown) {
+      if (err instanceof Error) {
         throw new Error(err.message)
       }
       throw new Error("Un Eror Desconocido ha Ocurrido")
     }
-    finally{
+    finally {
       setAddToCartLoader(false)
     }
 
   }
 
-  async function handleAddToWishlist(){
+  async function handleAddToWishlist() {
+    setWishListLoader(true)
     const formData = new FormData()
     formData.set("product_id", String(product.id))
     formData.set("email", loggedInUserEmail ? loggedInUserEmail : "")
 
-    try{
-      await addToWishlistAction(formData)
-      setAddedToWishList(curr => !curr)
-      toast.success("Producto Añadido a la lista de Deseos")
+    try {
+      const result = await addToWishlistAction(formData)
+      setAddedToWishList(result.action === "created")
+      toast.success("Lista de Deseos Actualizada")
     }
-    catch(err: unknown){
-      if(err instanceof Error){
+    catch (err: unknown) {
+      if (err instanceof Error) {
         throw new Error(err.message)
       }
       throw new Error("Un Eror Desconocido ha Ocurrido")
+    }
+    finally {
+      setWishListLoader(false)
     }
   }
 
   return (
     <div className="bg-gray-50 padding-x py-10 flex items-start flex-wrap gap-12 main-max-width padding-x mx-auto">
       {/* Product Image */}
-    
+
       <div className="w-[350px] h-[400px] relative overflow-hidden rounded-lg shadow-sm border border-gray-200">
         <Image
           src={`${BASE_URL}${product.image}`}
@@ -101,7 +106,7 @@ const ProductInfo = ({product, loggedInUserEmail}: {product: ProductDetail, logg
         <div className="flex flex-col gap-3">
           <h1 className="text-3xl font-bold">{product.name}</h1>
           <h3 className="text-2xl font-semibold text-black">${product.price}</h3>
-          
+
         </div>
 
         {/* Product Details */}
@@ -115,25 +120,24 @@ const ProductInfo = ({product, loggedInUserEmail}: {product: ProductDetail, logg
 
         {/* Buttons */}
         <div className='flex py-3 items-center gap-4 flex-wrap'>
-            <Button className="default-btn disabled:opacity-50 disabled:cursor-not-allowed" handleClick={handleAddToCart} disabled={addToCartLoader || addedToCart}>
-                {addToCartLoader ? "Añadiendo..." : addedToCart ? "Añadido al Carrito" : "Añadir al Carrito"}
-            </Button>
+          <Button className="default-btn disabled:opacity-50 disabled:cursor-not-allowed" handleClick={handleAddToCart} disabled={addToCartLoader || addedToCart}>
+            {addToCartLoader ? "Añadiendo..." : addedToCart ? "Añadido al Carrito" : "Añadir al Carrito"}
+          </Button>
 
-            {loggedInUserEmail ? (
-              <Button handleClick={handleAddToWishlist} className="wish-btn">
-                {addedToWishList ? "Quitar de la lista de Deseos" : "Añadir a lista de Deseos"}
+          {loggedInUserEmail ? (
+            <Button disabled={addWishListLoader} handleClick={handleAddToWishlist} className="wish-btn disabled:opacity-50 disabled:cursor-not-allowed">
+              {addedToWishList ? addWishListLoader ? "Actualizando..." : "Quitar de la lista de Deseos" : addWishListLoader ? "Agregando..." : "Añadir a lista de Deseos"}
             </Button>
-            )
+          )
             :
             (
-            <WishlistTooltip loggedInUserEmail={loggedInUserEmail}/>
+              <WishlistTooltip loggedInUserEmail={loggedInUserEmail} />
             )
-            }
+          }
         </div>
-        
+
       </div>
     </div>
   )
 }
-
 export default ProductInfo
